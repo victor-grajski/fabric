4590f4beccfa0f947dac51734aaf38d7959dd453
Olivia <oliviaseow@gmail.com>
2018-11-18 13:59:28 -0800

    import react-router

--- src/signuppage.js
+++ src/signuppage.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import firebase from './firebase.js';
 import './style.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class SignupPage extends React.Component {
   render() {
